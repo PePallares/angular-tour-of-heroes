633cfdca84eac83c5a20c821d822913651a9f2dd
PepPallares <ppallaresr@uoc.edu>
2024-05-10 00:21:49 +0200

    (feat) Added in memory data service

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,21 +1,19 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {NgIf, UpperCasePipe} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {Hero} from '../hero';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
+import { Location, NgIf, UpperCasePipe } from '@angular/common';
+
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  standalone: true,
   selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.scss'],
+  standalone: true,
   imports: [FormsModule, NgIf, UpperCasePipe],
+  templateUrl: './hero-detail.component.html',
+  styleUrls: [ './hero-detail.component.scss' ]
 })
-
 export class HeroDetailComponent implements OnInit {
-  
   hero: Hero | undefined;
 
   constructor(
@@ -27,14 +25,21 @@ export class HeroDetailComponent implements OnInit {
   ngOnInit(): void {
     this.getHero();
   }
-  
+
   getHero(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
   }
 }
