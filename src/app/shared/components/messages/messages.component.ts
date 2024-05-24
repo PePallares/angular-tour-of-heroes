fad7e18b795d08bd9d930ed66ce17c55b516346e
PepPallares <ppallaresr@uoc.edu>
2024-05-24 13:12:53 +0200

    (refactor) Beautify messages

--- src/app/shared/components/messages/messages.component.ts
+++ src/app/shared/components/messages/messages.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MessageService } from '../../../services/message/message.service';
 import { NgFor, NgIf } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [NgIf, NgFor, MatSidenavModule],
+  imports: [NgIf, NgFor, MatSidenavModule, MatDividerModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.scss'
 })
